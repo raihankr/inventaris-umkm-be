c614ca9861e6a9c067f0832a9c1e76f438fd7c9c
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2025-11-23 20:56:57 +0700

    Fix error on transaction

--- src/routes/transactions.route.js
+++ src/routes/transactions.route.js
@@ -1,7 +1,5 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/isAuthenticated.middleware.js";
-import { createTransaction } from "../controllers/transactions/createTransaction.controller.js";
 
 export const transactionRoutes = express.Router();
 
-transactionRoutes.post('/', isAuthenticated, createTransaction);
