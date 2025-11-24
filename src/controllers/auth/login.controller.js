8d49548cab6cea2469ccb2f61c4f869f369f4bc3
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2025-11-24 13:29:37 +0700

    Fix cookie problem

--- src/controllers/auth/login.controller.js
+++ src/controllers/auth/login.controller.js
@@ -15,7 +15,7 @@ export const login = async (req, res, next) => {
             path: '/',
             httpOnly: true,
             secure: NODE_ENV === "production" || NODE_ENV === "staging",
-            sameSite: "none",
+            sameSite: NODE_ENV === "development" ? "Lax" : "None",
             expires: new Date(Date.now() + 24 * 60 * 60 * 1000)
         };
 
